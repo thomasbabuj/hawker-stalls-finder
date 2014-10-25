9296e6ce3175cc14f9cb9b4b80aa1e5c3874abcb
Thomas Babu <tommy.developer@gmail.com>
2014-10-26 02:02:35 +0800

    removed the footer links

--- app/views/layouts/main.blade.php
+++ app/views/layouts/main.blade.php
@@ -107,11 +107,11 @@
     </div>
 
     <footer class="row">
-        <p class="col-md-9 col-sm-9 col-xs-12 copyright">&copy; <a href="http://usman.it" target="_blank">Muhammad
-                Usman</a> 2012 - 2014</p>
+        <p class="col-md-9 col-sm-9 col-xs-12 copyright">&copy; <a href="#" target="_blank">Choppe Lah
+                </a> 2014</p>
 
         <p class="col-md-3 col-sm-3 col-xs-12 powered-by">Powered by: <a
-                href="http://usman.it/free-responsive-admin-template">Charisma</a></p>
+                href="#"></a></p>
     </footer>
 
 </div><!--/.fluid-container-->
